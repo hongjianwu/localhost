a886957d26307da946da24dbf51db5223a947390
jianwu <jianwu@weiapp.com>
2018-12-17 17:39:37 +0800

    develop

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+error_reporting(E_ALL);
 \Swoole\Coroutine::create(function () {
     echo "hello go1 \n";
 });
 
 echo "hello main \n";
+
 \Swoole\Coroutine::create(function () {
     echo "hello go2 \n";
 });
