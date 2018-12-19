8784f894fa00eacfe9ffd9da0828d41d78d41e26
jianwu <jianwu@weiapp.com>
2018-12-19 18:19:33 +0800

    test

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 define('APPLICATION_PATH', dirname(dirname(__FILE__)));
-if (!file_exists(APPLICATION_PATH.'vendor/autoload.php')) {
+if (!file_exists(APPLICATION_PATH.'/vendor/autoload.php')) {
     echo '文件不存在';
 }
 
