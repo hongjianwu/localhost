1b89251ae75c6131ce0413f7e9eaafd313cdfdf2
jianwu <jianwu@weiapp.com>
2018-12-19 17:18:59 +0800

    develop

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
 
-echo __FILE__;
-echo dirname(dirname(__FILE__));
+dirname(dirname(__FILE__));
+
+$str = "a:1:{i:0;a:4:{s:2:\"id\";i:20717;s:9:\"image_url\";s:54:\"upload/images/201812/5RnoQsM1544334623joF14Nb20717.jpg\";s:5:\"width\";i:300;s:6:\"height\";i:1561;}}";
+var_dump(unserialize($str));exit;
