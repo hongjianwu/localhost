e204cdf837f6b7a4cc6cdb9ccc1ad660113ed177
jianwu <jianwu@weiapp.com>
2018-12-20 13:45:03 +0800

    develop

--- public/index.php
+++ public/index.php
@@ -9,4 +9,15 @@ if (!file_exists(APPLICATION_PATH.'/vendor/autoload.php')) {
 require_once APPLICATION_PATH . '/vendor/autoload.php';
 
 $app = new Slim\App();
-var_dump($app);exit;
+
+$app->get('/', function ($request, $response, $args) {
+    $response->write("Welcome to Slim!");
+    return $response;
+});
+
+$app->get('/hello[/{name}]', function ($request, $response, $args) {
+    $response->write("Hello, " . $args['name']);
+    return $response;
+})->setArgument('name', 'World!');
+
+$app->run();
