5d265ac0f698325d777d329f05da77b1acc558c2
jianwu <jianwu@weiapp.com>
2018-12-20 11:16:46 +0800

    develop

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 define('APPLICATION_PATH', dirname(dirname(__FILE__)));
+
 if (!file_exists(APPLICATION_PATH.'/vendor/autoload.php')) {
-    echo '文件不存在';
+    echo 'framework error' . PHP_EOL;
+    die();
 }
 
+require_once APPLICATION_PATH . '/vendor/autoload.php';
