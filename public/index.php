60172819db7270a700d04610f5e774838e9b0e67
jianwu <jianwu@weiapp.com>
2019-01-09 10:03:40 +0800

    develop

--- public/index.php
+++ public/index.php
@@ -9,18 +9,18 @@ if (!file_exists(APPLICATION_PATH.'/vendor/autoload.php')) {
 
 require_once APPLICATION_PATH . '/vendor/autoload.php';
 
-$app = new Slim\App();
-
-$app->get('/', function ($request, $response, $args) {
-    $response->write("Welcome to Slim!");
-    return $response;
-});
-
-$app->get('/hello[/{name}]', function ($request, $response, $args) {
-    $response->write("Hello, " . $args['name']);
-    return $response;
-})->setArgument('name', 'World!');
-
-$app->run();
+//$app = new Slim\App();
+//
+//$app->get('/', function ($request, $response, $args) {
+//    $response->write("Welcome to Slim!");
+//    return $response;
+//});
+//
+//$app->get('/hello[/{name}]', function ($request, $response, $args) {
+//    $response->write("Hello, " . $args['name']);
+//    return $response;
+//})->setArgument('name', 'World!');
+//
+//$app->run();
 
 $p = new \PhpOffice\PhpWord\PhpWord();
